4063af2cbe220104e0c16a4cfc87e9bf25e4cde4
Sai Lakshmi <sailakshmi22.p@gmail.com>
2022-09-30 23:04:12 -0400

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Sai Lakshmi on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
